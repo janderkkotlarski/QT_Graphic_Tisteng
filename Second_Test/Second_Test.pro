436515e054035e1d03502a30b1887b4b6de82ad2
janderkkotlarski <j.d.kotlarski@gmx.com>
2016-10-27 16:28:00 +0200

    New try

---
+++ Second_Test/Second_Test.pro
@@ -0,0 +1,22 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2016-10-27T10:33:40
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = Second_Test
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        mainwindow.cpp
+
+HEADERS  += mainwindow.h
+
+FORMS    += mainwindow.ui
+
+QMAKE_CXXFLAGS += -std=c++11 -Wall -Wextra
---
+++ Second_Test/mainwindow.cpp
@@ -0,0 +1,21 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    // QObject::connect(ui->pushButton,SIGNAL(clicked()),this,SLOT(OnClick()));
+
+    // QObject::connect(ui->checkBox,SIGNAL(toggled(bool)),this,SLOT(showMaximized()));
+    QObject::connect(ui->checkBox,SIGNAL(isChecked(bool)),this,SLOT(showMaximized()));
+    QObject::connect(ui->checkBox,SIGNAL(isUnchecked(bool)),this,SLOT(showMaximized()));
+    // QObject::connect(ui->checkBox,SIGNAL(released()),this,SLOT(showNormal()));
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
